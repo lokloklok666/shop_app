bdf05ee3063812c868b9e7a3a16065d318523cc3
nehcnijuw <1147904180@qq.com>
2020-01-17 01:26:27 +0800

    use element-ui

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <el-avatar class="margin-top margin-bottom"
+                   src="avatarIconUrl"
+                   @error="loadAvatarUrlErr">
+            <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png" />
+        </el-avatar>
+        <p>{{nickName}}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            nickName: 'nickName',
+            avatarIconUrl: 'https://empty',
+        }
+    },methods: {
+        loadAvatarUrlErr:function() {
+            return true;
+        },
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.width-auto {
+  margin: 0px;
+  width: 100%;
+  height: 100%;
+  zoom: 1;
+}
+
+.avatar-size {
+  max-width: 60px;
+  max-height: 60px;
+}
+
+.img-center {
+  vertical-align: middle;
+}
+.margin-top {
+  margin-top: 20px;
+}
+.margin-bottom {
+  margin-bottom: 5px;
+}
+.text-align-center {
+  text-align: center;
+  outline: none;
+}
+</style>
+
+
+
---
+++ src/views/MainContainer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <el-container>
+            <el-aside class="aside-box-shadow"
+                      width="200px">
+                <user-info></user-info>
+                <el-divider></el-divider>
+                <aside-nav></aside-nav>
+            </el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import UserInfo from '../components/UserInfo.vue'
+import AsideNav from '../components/AsideNav.vue'
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+        'user-info': UserInfo,
+        'aside-nav': AsideNav,
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.aside-box-shadow {
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
+
+
